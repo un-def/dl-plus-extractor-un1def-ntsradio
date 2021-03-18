682786b2b58ae543a3dc6a8375cb49fc3483a66f
un.def <un.def@ya.ru>
2021-03-18 11:17:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dl-plus-extractor-un1def-ntsradio
+
+A [dl-plus](https://github.com/un-def/dl-plus) extractor plugin for [NTS Radio](https://www.nts.live/)
+
+## Included extractors
+
+  * `un1def/ntsradio:episode` — episodes (`https://www.nts.live/shows/<show>/episodes/<episode>`)
---
+++ src/dl_plus/extractors/un1def/ntsradio.py
@@ -0,0 +1,75 @@
+from dl_plus import ytdl
+from dl_plus.extractor import Extractor, ExtractorError, ExtractorPlugin
+
+
+urljoin = ytdl.import_from('utils', 'urljoin')
+
+
+__version__ = '0.1.0.dev0'
+
+
+plugin = ExtractorPlugin(__name__)
+
+
+class NTSRadioBaseExtractor(Extractor):
+
+    DLP_BASE_URL = r'https?://(?:www\.)?nts\.live'
+
+    _API_BASE = 'https://www.nts.live/api/v2/'
+
+    def _fetch(self, *path, description, item_id, **kwargs):
+        """
+        Fetch the resource using NTS API.
+
+        The positional arguments are the parts of the resource path relative
+        to the _API_BASE.
+
+        The following keyword arguments are required by this method:
+            * item_id -- item identifier (for logging purposes).
+            * description -- human-readable resource description (for logging
+            purposes).
+
+        Any additional keyword arguments are passed directly to
+        the _download_json method.
+        """
+        response = self._download_json(
+            urljoin(self._API_BASE, '/'.join(path)),
+            item_id,
+            note=f'Downloading {description} metadata',
+            errnote=f'Unable to download {description} metadata',
+            **kwargs,
+        )
+        if not isinstance(response, dict):
+            raise ExtractorError(f'JSON object expected, got: {response!r}')
+        error = response.get('error')
+        if error:
+            raise ExtractorError(
+                f'{self.IE_NAME} returned error: {error}', expected=True)
+        return response
+
+
+@plugin.register('episode')
+class NTSRadioEpisodeExtractor(NTSRadioBaseExtractor):
+
+    DLP_REL_URL = (
+        r'/shows/(?P<show_id>[^/#?]+)/episodes/(?P<episode_id>[^/#?]+)')
+
+    def _real_extract(self, url):
+        show_id, episode_id = self.dlp_match(
+            url).group('show_id', 'episode_id')
+        episode = self._fetch(
+            'shows', show_id, 'episodes', episode_id,
+            item_id=episode_id, description='episode',
+        )
+        result = {
+            '_type': 'url_transparent',
+            'description': episode['description'],
+        }
+        for source in episode['audio_sources']:
+            if source['source'] == 'mixcloud':
+                result['url'] = source['url']
+                result['ie_key'] = 'Mixcloud'
+                break
+        else:
+            result['url'] = episode['audio_sources'][0]['url']
+        return result
